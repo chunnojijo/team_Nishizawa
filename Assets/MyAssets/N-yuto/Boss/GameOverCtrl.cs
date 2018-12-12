2168f4d71a1a556f6f507256482b6e3567276ada
yuto <iykuetboo@gmail.com>
2018-12-12 23:51:24 +0900

    gameoverCtrl.cs途中 完全透明からのブラックアウトしたい

---
+++ Assets/MyAssets/N-yuto/Boss/GameOverCtrl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverCtrl : MonoBehaviour {
+
+    [SerializeField]GameObject darkness;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Darken();
+        }
+        
+    }
+
+    void Darken()
+    {
+        MeshRenderer darkRenderer = darkness.GetComponent<MeshRenderer>();
+
+        darkRenderer.material.color = new Color(0f,0f,0f,0f);
+
+    }
+
+}
