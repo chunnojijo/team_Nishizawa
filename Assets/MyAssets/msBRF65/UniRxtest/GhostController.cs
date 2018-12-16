bc508404a1e43065ff272802e9abddfdf00fea2b
msBRF65 <msaito6525@gmail.com>
2018-12-16 14:31:26 +0900

    UniRx&MoveSystem

---
+++ Assets/MyAssets/msBRF65/UniRxtest/GhostController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class GhostController : MonoBehaviour {
+    private Subject<string> ColorSubject = new Subject<string>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
