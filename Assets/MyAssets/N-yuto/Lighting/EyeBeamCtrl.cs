d60aee0395e475c088f00b1f568b030dc2d7f6d3
yuto <iykuetboo@gmail.com>
2018-12-17 12:14:17 +0900

    Lighting　色変更実装

---
+++ Assets/MyAssets/N-yuto/Lighting/EyeBeamCtrl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeBeamCtrl : MonoBehaviour {
+
+    [SerializeField] private GameObject[] SpotLights;
+
+    public static int ColorMode = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("p"))
+        {
+            ChangeColorMode();
+        }
+		
+	}
+
+    void ChangeColorMode()
+    {
+        EyeBeamCtrl.ColorMode = (EyeBeamCtrl.ColorMode + 1) % 4;
+
+        for (int i = 0; i < SpotLights.Length; i++)
+        {
+            ChangeLightColor(SpotLights[i]);
+        }
+
+    }
+
+    void ChangeColorMode(int mode)
+    {
+        EyeBeamCtrl.ColorMode = mode;
+
+        for (int i = 0; i < SpotLights.Length; i++)
+        {
+            ChangeLightColor(SpotLights[i]);
+        }
+
+    }
+
+    void ChangeLightColor(GameObject light)
+    {
+        Color c;
+
+        switch (EyeBeamCtrl.ColorMode)
+        {
+            case 1: c = new Color(1,0.2f,0.2f,1); break;
+            case 2: c = Color.cyan; break;
+            case 3: c = Color.yellow; break;
+            default: c = Color.white; break;
+        }
+
+        light.GetComponent<Light>().color = c;
+
+    }
+
+
+
+}
