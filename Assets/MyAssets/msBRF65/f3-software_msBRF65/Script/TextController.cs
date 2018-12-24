406416ee0242045aa5fd808a5d27a75fc07441c8
msBRF65 <msaito6525@gmail.com>
2018-12-24 14:44:57 +0900

    途中の進捗

---
+++ Assets/MyAssets/msBRF65/f3-software_msBRF65/Script/TextController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    // Use this for initialization
+    string[] text;
+    string all_serif;
+    public GameObject image;
+    int i = 0;
+    public TextAsset serif;
+    ImageController Image;
+
+    void Start()
+    {
+        all_serif = serif.text;
+        text = all_serif.Split('\n');
+        Image = image.GetComponent<ImageController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void UI_Update()
+    {
+        i++;
+        if (i < text.Length - 1 && text[i] != text[0])//text[0]はpause、最後は空白
+        {
+            this.GetComponent<Text>().text = "";
+            image.gameObject.SetActive(true);
+            StartCoroutine(Image.Action());
+            Invoke("WriteUI", (1 / Image.speed + Image.marge * 2) * Time.deltaTime + 0.1f);//関数の発生を遅らせる
+        }
+        else
+        {
+            image.gameObject.SetActive(false);
+            this.GetComponent<Text>().text = "";
+        }
+    }
+
+    //textboxの大きさを調整(vector3でコピー元との相対的な大きさを操作)
+    public void Scale(Vector3 scale)
+    {
+        this.gameObject.GetComponent<Text>().rectTransform.localScale = scale;
+    }
+
+    //UIへの書き込み
+    void WriteUI()
+    {
+        this.GetComponent<Text>().text = text[i];
+
+    }
+}
