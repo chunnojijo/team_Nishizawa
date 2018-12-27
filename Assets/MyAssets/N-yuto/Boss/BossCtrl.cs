d8abcc1d7d39706197a0afd8c035846ece365af3
yuto <iykuetboo@gmail.com>
2018-12-27 16:51:32 +0900

    Atagoのファイルいじっちゃったーーーー

--- Assets/MyAssets/N-yuto/Boss/BossCtrl.cs
+++ Assets/MyAssets/N-yuto/Boss/BossCtrl.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 
 public class BossCtrl : MonoBehaviour {
 
-    float time = 100;
-    Animation anim;
+    float time = 30;
+    [SerializeField] GameObject BossMesh;
+    [SerializeField] GameObject Cabinett;
+    Animation anim_boss;
+    Animator anim_cabinett;
 
-	// Use this for initialization
-	void Start () {
-        anim = gameObject.GetComponent<Animation>();
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    IEnumerator coroutine;
+
+    // Use this for initialization
+    void Start () {
+
+        coroutine = BossEnumerator();
+
+        anim_boss = BossMesh.GetComponent<Animation>();
+        anim_cabinett = Cabinett.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         if (Input.GetKeyDown("m"))
         {
-            StartCoroutine("BossEnumerator");
+            StartCoroutine(coroutine);
 
         }
     }
@@ -26,9 +35,24 @@ public class BossCtrl : MonoBehaviour {
 
     private IEnumerator BossEnumerator()
     {
-        //anim.Play("appear");
+        anim_cabinett.Play("CabinettOpen");
 
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(1.3f);
+
+        anim_boss.Play("appear_noRot");
+
+        yield return new WaitForSeconds(1.5f);
+
+        float t = 0;
+
+        while(true)
+        {
+            BossMesh.transform.localRotation = Quaternion.Euler(-90,0,Mathf.Sin(t * Mathf.PI) * 40);
+            t += 0.3f * Time.deltaTime;
+            yield return null;
+        }
+
+        yield break;
 
         iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("BossPath1"),
                       "time", time, "easeType", iTween.EaseType.linear, "orienttopath", true));
