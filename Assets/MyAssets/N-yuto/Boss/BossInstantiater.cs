5c1b0ebe3ed4a8dba86b4f675b27752956f4cfd3
yuto <iykuetboo@gmail.com>
2018-12-28 02:37:25 +0900

    GameOver処理があかん Bossの入れ替えがPrefab式だと無理そう >入れ替えず状態だけリセットを模索しようかな

---
+++ Assets/MyAssets/N-yuto/Boss/BossInstantiater.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossInstantiater : MonoBehaviour {
+
+    [SerializeField] GameObject CabinettPrefab; 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("n"))
+        {
+            ResetBoss();
+        }
+
+
+	}
+
+    public void ResetBoss()
+    {
+        Instantiate(CabinettPrefab);
+    }
+}
