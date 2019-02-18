1169cbca35dde6f9d4337efa1aa126eb8b02abda
yuto <iykuetboo@gmail.com>
2019-02-18 11:38:56 +0900

    Navi は レベル 6 に あがった！ Navi は GoTo(TargetPoint) を おぼえた！

---
+++ Assets/MyAssets/N-yuto/Navi/NaviVoice.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaviVoice : MonoBehaviour {
+
+    [SerializeField] AudioSource source;
+
+    [SerializeField] AudioClip[] clips ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play(int ClipNumber)
+    {
+        if(ClipNumber >= clips.Length || ClipNumber < 0) { return; }
+
+        source.PlayOneShot(clips[ClipNumber]);
+    }
+
+}
