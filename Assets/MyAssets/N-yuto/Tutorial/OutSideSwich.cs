557ede8737a7e721016d181347bec7d602e4f46b
yuto <iykuetboo@gmail.com>
2019-02-19 17:21:33 +0900

    Tutorial用ステージ完成

---
+++ Assets/MyAssets/N-yuto/Tutorial/OutSideSwich.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OutSideSwich : MonoBehaviour {
+
+    public UnityEvent ON_OpeningMode;
+    public UnityEvent ON_EndingMode;
+    public UnityEvent OFF;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
