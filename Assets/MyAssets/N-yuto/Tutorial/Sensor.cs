d5b99eeddb86ea155448196a0196c21778365d87
yuto <iykuetboo@gmail.com>
2019-02-21 12:36:24 +0900

    屋外でのOpeningほぼ完成！！

---
+++ Assets/MyAssets/N-yuto/Tutorial/Sensor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Sensor : MonoBehaviour {
+
+    [SerializeField] UnityEvent FindPlayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.tag == "Player")
+        {
+            FindPlayer.Invoke();
+        }
+
+
+
+
+
+    }
+}
