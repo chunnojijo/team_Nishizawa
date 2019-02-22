0663d073692e41d254805f4818bcfc8a5aad671a
atagorv <kogata513@gmail.com>
2019-02-23 08:15:34 +0900

    グラブ、ドア閉め、部屋ギミック１（天井落下）

---
+++ Assets/MyAssets/atago/Scripts/FallCeiling.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallCeiling : MonoBehaviour {
+
+    public GameObject[] objects;
+
+    // Use this for initialization
+    void Start()
+    {
+        for(int i=0; i<objects.Length; i++)
+        {
+            objects[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            for(int i=0; i<objects.Length; i++)
+            {
+                objects[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            }
+
+            gameObject.SetActive(false);
+        }
+    }
+}
