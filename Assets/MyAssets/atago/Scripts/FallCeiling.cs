d5d1922466a08df45d7ee5c6c4509a1dfa3e2a3b
atagorv <kogata513@gmail.com>
2019-02-25 14:07:56 +0900

    引き出し実装途中

--- Assets/MyAssets/atago/Scripts/FallCeiling.cs
+++ Assets/MyAssets/atago/Scripts/FallCeiling.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class FallCeiling : MonoBehaviour {
 
-    public GameObject[] objects;
+    public GameObject[] falls;
+    public GameObject[] walls;
 
     // Use this for initialization
     void Start()
     {
-        for(int i=0; i<objects.Length; i++)
+        for(int i=0; i<falls.Length; i++)
         {
-            objects[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            falls[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         }
         
     }
@@ -20,12 +21,23 @@ public class FallCeiling : MonoBehaviour {
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            for(int i=0; i<objects.Length; i++)
+            for(int i=0; i<falls.Length; i++)
             {
-                objects[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                falls[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
             }
+
+            Invoke("CreateWall", 2f);
 
             gameObject.SetActive(false);
         }
+    }
+
+    void CreateWall()
+    {
+        for (int i = 0; i < walls.Length; i++)
+        {
+            walls[i].SetActive(true);
+        }
+
     }
 }
