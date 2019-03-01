2cea29ebcf8ea9d4ad17387dce49d24074c25e4f
Yamaguchi Shuntaro <chunnojijo@gmail.com>
2019-03-01 19:04:25 +0900

    部屋の一部が完成

---
+++ Assets/MyAssets/Chun/Script/FireSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSound : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.GetComponent<Torchelight>().IntensityLight > 0)
+        {
+            this.GetComponent<AudioSource>().volume = 0.385f;
+        }
+	}
+}
---
+++ Assets/MyAssets/Chun/Script/GhostPictureDisolve.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostPictureDisolve : MonoBehaviour {
+
+    private float time = 0;
+    private bool disolve = false;
+    private Renderer _renderer;
+
+    // Use this for initialization
+    void Start () {
+        _renderer = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (time > 2)
+        {
+            disolve = true;
+        }
+
+        if (disolve)
+        {
+            _renderer.material.EnableKeyword("_Threshold");
+        }
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Torch")
+        {
+            time += Time.deltaTime;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Torch")
+        {
+            time = 0;
+        }
+    }
+}
