d820f4f6997508a4dc9895f5a78000495260ee61
Yamaguchi Shuntaro <chunnojijo@gmail.com>
2019-03-01 19:04:25 +0900

    部屋の一部が完成

---
+++ Assets/MyAssets/Chun/Script/LockAnimationEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockAnimationEvent : MonoBehaviour {
+    private AudioSource[] audiosource;
+	// Use this for initialization
+	void Start () {
+        audiosource = GetComponents<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void keyOpen()
+    {
+        audiosource[0].Play();
+    }
+    private void jouOpen()
+    {
+        audiosource[1].Play();
+    }
+}
---
+++ Assets/MyAssets/Chun/Script/SwitchAnimationEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchAnimationEvent : MonoBehaviour {
+
+    private AudioSource audiosource;
+	// Use this for initialization
+	void Start () {
+        audiosource = GetComponent<AudioSource>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SwitchPush()
+    {
+        audiosource.Play();
+    }
+}
