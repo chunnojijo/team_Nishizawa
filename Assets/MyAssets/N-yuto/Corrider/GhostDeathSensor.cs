3595681c4836469f35fec244694f4bd15ad610b8
yuto <iykuetboo@gmail.com>
2019-03-22 10:51:00 +0900

    おおおじょじょじょじょじょじょじょじょじょ

---
+++ Assets/MyAssets/N-yuto/Corrider/GhostDeathSensor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostDeathSensor : MonoBehaviour {
+
+    Escape escape;
+    [SerializeField] GameObject key;
+    
+	// Use this for initialization
+	void Start () {
+        escape = gameObject.GetComponent<Escape>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (escape.dieatall)
+        {
+            key.SetActive(true);
+        }
+	}
+
+
+
+}
